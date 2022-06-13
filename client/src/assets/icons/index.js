6021ebd2cb62bab30c9c35704335498d971e9bf2
siddhantprateek <siddhantprateek@gmail.com>
2022-06-13 19:21:36 +0530

    icon default export

---
+++ client/src/assets/icons/index.js
@@ -0,0 +1,8 @@
+export { default as HistoryIcon } from './history.svg';
+export { default as CartIcon } from './cart.svg';
+export { default as OffersIcon } from './offers.svg';
+export { default as SettingIcon } from './settings.svg';
+export { default as WalletIcon } from './wallet.svg';
+export { default as WatchListIcon } from './watchlist.svg';
+export { default as OverviewIcon } from './Overview.svg'
+export { default as SearchIcon } from './search.png';
