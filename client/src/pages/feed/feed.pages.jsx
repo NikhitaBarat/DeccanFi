46f695f415530b6a0de50a9861c2c2c0a0ab098e
Nikhita <bnikhita2003@gmail.com>
2022-06-13 20:15:07 +0530

    modified feed

---
+++ client/src/pages/feed/feed.pages.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Feed = () => {
+  return (
+    <div className='feed-page'>
+      <div className="feed-bg">
+      
+      </div>
+    
+    </div>
+  )
+}
+
+export default Feed
