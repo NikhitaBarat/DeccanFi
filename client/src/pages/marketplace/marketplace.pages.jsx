dd23d329cadfa3c757e7b8bc57be607ea90e8cbf
siddhantprateek <siddhantprateek@gmail.com>
2022-06-13 19:25:32 +0530

    market pages created

---
+++ client/src/pages/marketplace/marketplace.pages.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Marketplace = () => {
+  return (
+    <div className='market-page'>
+      <div className="market-bg">
+      
+      </div>
+    
+    </div>
+  )
+}
+
+export default Marketplace
