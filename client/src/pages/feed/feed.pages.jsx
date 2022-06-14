38372c894d72221db599f5e775470a95387694e7
siddhantprateek <siddhantprateek@gmail.com>
2022-06-14 11:29:32 +0530

    feed page created

---
+++ client/src/pages/feed/feed.pages.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Feed = () => {
+  return (
+    <div className="feed-page">
+      <div className="feed-bg">
+        <h1>FEED</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Feed;
