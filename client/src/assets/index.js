70358713fd4bc8129e447602f4ed291b29f2d46e
siddhantprateek <siddhantprateek@gmail.com>
2022-06-14 18:27:42 +0530

    footer default exported

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -3,3 +3,4 @@ export { default as MonsterRight } from './monsterright.png';
 export { default as Logo } from './logo.png';
 export { default as BoredApes } from './bored-apes.png';
 export { default as Babymonster } from './babymonster.png';
+export { default as GrpLogo } from './grplogo.png';
