fc7a10bf9fd94ed1c91abfdecd5b346b352a5373
siddhantprateek <siddhantprateek@gmail.com>
2022-06-15 14:32:19 +0530

    route for dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Dashboard, Home, Feed, Stat, Profile } from './pages';
 import { Header, Footer } from './components';
-
 function App() {
+
   return (
     <div className="App">
       <Header />
       <Routes>
         <Route index element={<Home />}/>
-        <Route path="dashboard" element={<Dashboard />}/>
+        <Route path="dashboard" element={<Dashboard />}>
+          <Route path="wallet" element={<Dashboard />} />
+        </Route>
         <Route path="feed" element={<Feed />}/>
         <Route path="profile" element={<Profile />}/>
         <Route path="stat" element={<Stat />}/>
