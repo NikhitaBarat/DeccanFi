e2ef2e21af530343f3af1e43e7e878f62604d846
Nikhita <bnikhita2003@gmail.com>
2022-06-15 21:11:30 +0530

    added backend

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
 const express = require('express')
-const axios = require('axios');
+const dotenv = require('dotenv')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const routeGraphQL = require('./graphql/query');
+dotenv.config()
+
+// configuration
 const app = express()
-const port = 4000
+const PORT = process.env.PORT || 4000
 
-app.use()
+// middleware
+app.use(express.json())
+app.use(cors())
 
+// routes
 app.get('/', (req, res) => {
-  res.send('nikhitaaaaaaaaaaaaa')
+    res.send('Server is running fine')
 })
 
-app.get('/api', async (req, res) =>{
-    const response = await axios.get('https://jsonplaceholder.typicode.com/todos/')
-    res.send(response)
+// database connection
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+db.once('open', () => {
+    console.log('Database is Connected')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+app.use('/graphql', routeGraphQL)
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`)
+})
