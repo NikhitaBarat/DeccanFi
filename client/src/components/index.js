b198d0b022060ed6db1c49a963dbbf0267e66cb9
Nikhita <bnikhita2003@gmail.com>
2022-06-15 21:42:35 +0530

    changed

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -7,4 +7,5 @@ export { default as AuctionNft } from "./auctionNft/auctionNft.components";
 export { default as Sponsors } from "./sponsers/sponsers.components";
 export { default as Story } from './story/story.components';
 export { default as Storycard } from './storycard/storycard.components';
+export { default as Recentcard } from './recentcard/recentcard.components';
 export { default as FeedProfileCard } from './feedProfileCard/feedProfileCard.components'; 
