d9cee82982eeb9de9e30d4c8671c91ebdd35aa9a
siddhantprateek <siddhantprateek@gmail.com>
2022-06-16 13:42:42 +0530

    setting page

---
+++ client/src/components/settings/setting.components.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './setting.styles.css';
+
+const Setting = () => {
+  return (
+    <div className='setting-page'>
+        <h1>Setting</h1>  
+    </div>
+  )
+}
+
+export default Setting
